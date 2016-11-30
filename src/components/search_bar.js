79a4fd901e48474c7caf5681c75906a485941df1
James Heywood <jdheywood@gmail.com>
2016-11-30 10:53:00 +0000

    Initial search bar wire up

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,13 +10,18 @@ class SearchBar extends Component {
   render() {
     // class based component can ref this.props anywhere
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })}
+          onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
